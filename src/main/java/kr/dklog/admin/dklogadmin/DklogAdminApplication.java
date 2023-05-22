f884c55b4e615b2b2c6410a8be358a0b0896e412
cheoljin408 <kcj409@naver.com>
2023-05-22 16:29:55 +0900

    first commit

---
+++ src/main/java/kr/dklog/admin/dklogadmin/DklogAdminApplication.java
@@ -0,0 +1,13 @@
+package kr.dklog.admin.dklogadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DklogAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DklogAdminApplication.class, args);
+    }
+
+}
