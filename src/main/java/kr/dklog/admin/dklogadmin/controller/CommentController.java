b13f6c4e982af7467021d0a7e4c6e0063a85b775
9noeyni9 <koo949494@naver.com>
2023-05-29 04:33:31 +0900

    feat: 댓글 리스트 조회 구현
    - 필요없는 annotation 삭제
    
    #14

--- src/main/java/kr/dklog/admin/dklogadmin/controller/CommentController.java
+++ src/main/java/kr/dklog/admin/dklogadmin/controller/CommentController.java
@@ -8,7 +8,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.awt.print.Pageable;
 import java.util.List;
 
 @RequiredArgsConstructor
